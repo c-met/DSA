3e5fb1709feb387363aa4ad417e418e60619400b
c-met <soumikgh60@gmail.com>
2023-09-29 20:03:23 +0530

    this is super keywords

---
+++ supers.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class supers {
+    public static void main(String[] args) {
+    Horse h = new Horse();
+    System.out.println(h.color);
+    }
+    
+}
+
+class Animal {
+    String color;
+    Animal() {
+        System.out.println("animal constructor is called");
+    }
+}
+
+class Horse extends Animal {
+    Horse() {
+        super.color = "brown";
+        System.out.println("Horse constructor is called");
+    }
+}
